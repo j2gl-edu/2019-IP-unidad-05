2187fc78c2307f8c43cabfb78a1544a902ac5f8e
Juan J. Garcia <j2gl.edu@gmail.com>
2019-08-29 16:35:16 -0600

    Clean up

--- src/main/java/TestColor.java
+++ src/main/java/TestColor.java
@@ -6,9 +6,11 @@ public class TestColor {
 
         // crea un objeto llamado verde, de la clase Color, enviandole el R,G,B
         Color verde = new Color(17, 194, 7);
+        
         // ejecuta el metodo getBlue del objeto verde, que obtiene el valor azul del color
         int azul = verde.getBlue();
         System.out.println("Valor azul para el verde  " + azul);
+
         // obtiene a partir del color creado, un verde mas oscuro, llamando al metodo darker()
         Color verdeOscuro = verde.darker();
         System.out.println(verdeOscuro);
